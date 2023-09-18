9449131ccdce4738e71fc8c04facc127822425b8
CekicN <cekicnikola53@gmail.com>
2023-09-19 00:55:58 +0200

    final

--- grow-together-api/src/admin/admin.module.ts
+++ grow-together-api/src/admin/admin.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
-
+import {UserModule} from './../user/user.module'
 @Module({
+  imports:[UserModule],
   controllers: [AdminController]
 })
 export class AdminModule {}
